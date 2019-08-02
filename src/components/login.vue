f97abee5bf4d5eae06ca1715304977c39c9aeb06
chongshan.tian01 <chongshan.tian01@weimob.com>
2019-08-02 17:14:52 +0800

    login

--- src/components/login.vue
+++ src/components/login.vue
@@ -5,12 +5,11 @@
       <Form ref="formLogin" :model="formLogin" :rules="ruleLogin">
       <div class="title">后台登录</div>
       <div>
-
         <FormItem prop="username">
-           <input v-model="formLogin.username" class="myinput" type="text" placeholder="请输入用户名" v-model="username" />
+           <input v-model="formLogin.username" class="myinput" type="text" placeholder="请输入用户名"  />
         </FormItem>
         <FormItem prop="password">
-           <input v-model="formLogin.password" @keyup.13="login" class="myinput" type="password" placeholder="请输入密码" v-model="password" />
+           <input v-model="formLogin.password" @keyup.13="login" class="myinput" type="password" placeholder="请输入密码"  />
         </FormItem>
       </div>
       <div class="login_other">
@@ -19,7 +18,7 @@
         <label for="remenberme">记住我</label>
       </div>
         <FormItem>
-            <button :disabled="disablebtn" class="login" @click="login('formLogin')" >{{loginText}}</button>
+            <Button :disabled="disablebtn" class="login" @click="login('formLogin')" >登录</Button>
         </FormItem>
       </Form>
     </div>
@@ -29,52 +28,65 @@
     import {
         Toast
     } from 'mint-ui';
+    import {
+        mapMutations
+    } from 'vuex';
+
     export default {
         name: 'backlogin',
         data() {
             return {
                 formLogin: {
-                    username: "admin",
-                    password: "123456",
+                    username: null,
+                    password: null,
                     disablebtn: false,
                     loginText: "登录"
                 },
-                ruleLogin:{
-                    username:[
-
+                ruleLogin: {
+                    userName: [
+                        {required: true, message: '请填写用户名', trigger: 'blur'}
                     ],
-                    password:[
-
+                    password: [
+                        {required: true, message: '请填写密码', trigger: 'blur'},
                     ]
                 }
             }
         },
         methods: {
+          ...mapMutations(['changeLogin']),
             login(formLogin) {
-                var vm = this;
-                this.disablebtn = true;
-                this.loginText = "登陆中";
-                this.$reqs.post('/users/login', {
-                    username: this.username,
-                    password: this.password
-                }).then(function (res) {
-                    if (res.data.status === true) {
-                        vm.$router.replace('/home');
-                    } else {
-                        //弹窗
-                        Toast({
-                            message: res.data.errMsg,
-                        });
-                        vm.disablebtn = false;
-                        vm.loginText = "登陆";
+                let _this = this;
+                if(this.loginFrom.userName === ''|| this.loginFrom.password === ''){
+                    alert('账号或密码不能为空');
+                }else{
+                    this.axios({
+                        methos: 'post',
+                        url: '/sys/user/login',
+                        data: _this.loginFrom
+                    }).then.(res=>{
+                        console.log(res.data);
+                        _this.userToken = 'Bearer' + res.data.data.body.token;
+                        //将用户token保存到vuex
+                        _this.changeLogin({Authorization:_this.userToken});
+                        _this.$router.push('/home');
+                        alert('登录成功')
+                    }).catch(error => {
+                        alert('账号或密码错误');
+                        console,log(error);
+                    });
+                }
+                /*this.$refs[formLogin].validate((valid) => {
+                    if (valid) {
+                        this.$store.dispatch('users/userLogin', {
+                            "user_name": this.formLogin.userName,
+                            "user_password": this.formLogin.password,
+                            "router": this.$router
+                        })
                     }
-                }).catch(function (err) {
-                    vm.disablebtn = false;
-                    vm.loginText = "登陆";
-                })
+                })*/
             }
         }
-    }
+    };
 </script>
 <style scope>
  /* 剩下的样式自由发挥*/

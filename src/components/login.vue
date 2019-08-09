40186dcb77b2d95ac261444b26b5ac1a04b254d6
chongshan.tian01 <chongshan.tian01@weimob.com>
2019-08-09 16:45:32 +0800

    update

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,26 +2,43 @@
   <div class="backlogin">
 
     <!-- 登录表单 -->
-    <el-form :model="login" status-icon :rules="rule" ref="login" class="login-box">
-      <h3 class="login-title">欢迎登录</h3>
-      <el-form-item prop="username">
+    <el-form :model="login" status-icon :rules="rule" ref="login"   class="login-box">
+      <el-row type="flex" justify="center">
+        <el-col :span="5">
+             <h3 class="login-title">欢迎登录</h3>
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="center">
+        <el-col :span="10">
+      <el-form-item label="用户名:" prop="username" >
         <el-input prefix-icon="el-icon-ump-yonghu" v-model="login.username"
                   auto-complete="off"/>
       </el-form-item>
-      <el-form-item prop="password">
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="center">
+        <el-col :span="10">
+      <el-form-item label="密码:" prop="password">
         <el-input prefix-icon="el-icon-ump-mima" type="password" v-model="login.password"
                   auto-complete="off"/>
       </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row class="pattern" type="flex" justify="center">
+        <el-col :span="5">
       <el-form-item>
         <el-checkbox class="check" v-model="checked">记住我</el-checkbox>
       </el-form-item>
       <el-form-item>
-        <el-button class="btn" type="primary" @click="submitForm('login')">登录</el-button>
+        <el-button class="btn login" type="primary" @click="submitForm('login')">登录</el-button>
       </el-form-item>
+      <el-form-item>
+        <el-button class="btn login" type="primary" @click="register">注册</el-button>
+      </el-form-item>
+        </el-col>
+      </el-row>
     </el-form>
-    <div>
-      <p><a href="/register" class="tips">还没有账号？点我去注册</a></p>
-    </div>
+
   </div>
 </template>
 <script>
@@ -44,6 +61,7 @@
             };
 
             return {
+                labelPosition:'right',
                 checked:false,
                 token: '',
                 login: {
@@ -61,6 +79,9 @@
             }
         },
         methods: {
+            register() {
+                this.$router.replace('/register')
+            },
             submitForm(login) {
                 this.$refs[login].validate((valid) => {
                     if (valid) {
@@ -90,14 +111,12 @@
     };
 </script>
 <style scope>
-  #backlogin{
 
-  }
 
   .login-box {
     border: 1px solid #DCDFE6;
-    width: 350px;
-    margin: 180px auto;
+    width: 800px;
+    margin: 100px auto;
     padding: 35px 35px 15px 35px;
     border-radius: 5px;
     -webkit-border-radius: 5px;
@@ -110,4 +129,9 @@
     margin: 0 auto 40px auto;
     color: #303133;
   }
+
+  .login{
+    position : relative;
+  }
+
 </style>

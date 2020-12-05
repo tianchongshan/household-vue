1a1af4b67b5838b225abd62756d5520ec21c82d8
tcs <>
2020-12-05 13:15:05 +0800

    23

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
 
-
+edasds
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
